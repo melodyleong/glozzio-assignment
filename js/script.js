1b7f89d913ce081562773f5e27ce81d425ec11d6
melodyleong <ziqingg.lzq@gmail.com>
2024-07-02 22:24:03 +0800

    2/7

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let slideInterval;
 
     function goToNextSlide() {
-        nextButton.click(); // Simulate click for Bootstrap or similar library handling
+        nextButton.click();
     }
 
     function startSlideShow() {
@@ -18,37 +18,5 @@ document.addEventListener('DOMContentLoaded', function () {
     function closeAnnouncement() {
         document.getElementById('announcementBar').style.display = 'none';
     }
-
-    //product expansion functionality
-    var rose3Btn = document.getElementById('rose3');
-var odette3Btn = document.getElementById('odette3');
-var kiko3Btn = document.getElementById('kiko3');
-// Get close button elements for each modal
-var rose3Close = rose3Modal.querySelector('.close-button');
-var odette3Close = odette3Modal.querySelector('.close-button');
-var kiko3Close = kiko3Modal.querySelector('.close-button');
-// Open the modal - Add event listeners to buttons
-rose3Btn.onclick = function () {
-    rose3Modal.style.display = "block";
-}
-odette3Btn.onclick = function () {
-    odette3Modal.style.display = "block";
-}
-kiko3Btn.onclick = function () {
-    kiko3Modal.style.display = "block";
-}
-// Close the modal - Add event listeners to close buttons
-rose3Close.onclick = function () {
-    rose3Modal.style.display = "none";
-}
-odette3Close.onclick = function () {
-    odette3Modal.style.display = "none";
-}
-kiko3Close.onclick = function () {
-    kiko3Modal.style.display = "none";
-}
-
-
-
 
 });
